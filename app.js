d8eda3145c86eb338005849428f3b717a49d4036
松田 信介 <matsusin0413@gmail.com>
2018-08-12 02:07:59 +0900

    first commit

---
+++ app.js
@@ -0,0 +1,70 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+var request = require('request');
+var crypto = require("crypto");
+var async = require('async');
+var sender = require('./sendMessage.js');
+var message = require('./messageTemplate.js');
+
+app.set('port', (process.env.PORT || 8000));
+// JSONの送信を許可
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+// JSONパーサー
+app.use(bodyParser.json());
+
+app.post('/callback', function(req, res) {
+    async.waterfall([
+            function(callback) {
+                // リクエストがLINE Platformから送られてきたか確認する
+                if (!validate_signature(req.headers['x-line-signature'], req.body)) {
+                    return;
+                }
+                // テキストが送られてきた場合のみ返事をする
+                if ((req.body['events'][0]['type'] != 'message') || (req.body['events'][0]['message']['type'] != 'text')) {
+                    return;
+                }
+                // 特定の単語に反応させたい
+                //if (req.body['events'][0]['message']['text'].indexOf('please input some word') == -1) {
+                //    return;
+                //}
+
+                // ユーザIDを取得する
+                var user_id = req.body['events'][0]['source']['userId'];
+                if (req.body['events'][0]['source']['type'] == 'user') {
+                    request.get(getProfileOption(user_id), function(error, response, body) {
+                        if (!error && response.statusCode == 200) {
+                            callback(req, body['displayName']);
+                        }
+                    });
+                }
+            },
+        ],
+        function(req, displayName) {
+            sender.send(req, [message.textMessage("こんにちは、" +　displayName　+ "さん")]);
+            return;
+        }
+    );
+});
+
+app.listen(app.get('port'), function() {
+    console.log('Node app is running');
+});
+
+function getProfileOption(user_id) {
+    return {
+        url: 'https://api.line.me/v2/bot/profile/' + user_id,
+        proxy: process.env.FIXIE_URL,
+        json: true,
+        headers: {
+            'Authorization': 'Bearer {' + process.env.LINE_CHANNEL_ACCESS_TOKEN + '}'
+        }
+    };
+}
+
+// 署名検証
+function validate_signature(signature, body) {
+    return signature == crypto.createHmac('sha256', process.env.LINE_CHANNEL_SECRET).update(new Buffer(JSON.stringify(body), 'utf8')).digest('base64');
+}
